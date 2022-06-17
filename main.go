895ebe13b4718751d17933d99996de39c4669dec
João Hudson <joao.oliveira@projecti.com.br>
2022-06-17 16:00:49 -0300

    reformulando modelos

--- main.go
+++ main.go
@@ -8,9 +8,19 @@ import (
 )
 
 type Dino struct {
-	Name   string
-	Owner  string
-	Action string
+	Id         uint64 `json:"id"`
+	Name       string `json:"name"`
+	Region     string `json:"region"`
+	Locomotion string `json:"locomotion"`
+	Food       string `json:"food"`
+	Training   string `json:"training"`
+	Utility    string `json:"utility"`
+}
+
+type DinoCategpryResponse struct {
+	Regions     []string `json:"regions"`
+	Locomotions []string `json:"locomotions"`
+	Foods       []string `json:"foods"`
 }
 
 const DefaultInternalServerErrorMessage = "Erro interno do servidor, por favor tente mais tarde."
@@ -19,9 +29,8 @@ func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 
-	http.HandleFunc("/dino", dinos)
-	http.HandleFunc("/owner", owners)
-	http.HandleFunc("/dino/name", dinoNames)
+	http.HandleFunc("/api/dino", dinos)
+	http.HandleFunc("/api/dino/category", dinoCategories)
 
 	err := http.ListenAndServe(":8081", nil)
 	if err != nil {
@@ -29,8 +38,12 @@ func main() {
 	}
 }
 
-func dinoNames(response http.ResponseWriter, request *http.Request) {
-	result := []string{}
+func dinoCategories(response http.ResponseWriter, request *http.Request) {
+	result := DinoCategpryResponse{
+		Regions:     []string{},
+		Locomotions: []string{},
+		Foods:       []string{},
+	}
 
 	data, err := readData("data.json")
 	if err != nil {
@@ -40,36 +53,25 @@ func dinoNames(response http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	set := make(map[string]bool)
+	regionSet := make(map[string]bool)
+	locomotionSet := make(map[string]bool)
+	foodSet := make(map[string]bool)
 	for i := range data {
-		set[data[i].Name] = true
+		regionSet[data[i].Region] = true
+		locomotionSet[data[i].Locomotion] = true
+		foodSet[data[i].Food] = true
 	}
 
-	for k := range set {
-		result = append(result, k)
+	for k := range regionSet {
+		result.Regions = append(result.Regions, k)
 	}
 
-	sendJson(response, result)
-}
-
-func owners(response http.ResponseWriter, request *http.Request) {
-	result := []string{}
-
-	data, err := readData("data.json")
-	if err != nil {
-		response.WriteHeader(http.StatusInternalServerError)
-		response.Write([]byte(DefaultInternalServerErrorMessage))
-		fmt.Println("Erro ao ler arquivo json: ", err)
-		return
+	for k := range locomotionSet {
+		result.Locomotions = append(result.Locomotions, k)
 	}
 
-	set := make(map[string]bool)
-	for i := range data {
-		set[data[i].Owner] = true
-	}
-
-	for k := range set {
-		result = append(result, k)
+	for k := range foodSet {
+		result.Foods = append(result.Foods, k)
 	}
 
 	sendJson(response, result)
@@ -87,14 +89,18 @@ func dinos(response http.ResponseWriter, request *http.Request) {
 		return
 	}
 
-	owner := request.URL.Query().Get("owner")
+	region := request.URL.Query().Get("region")
+	locomotion := request.URL.Query().Get("locomotion")
+	food := request.URL.Query().Get("food")
 	name := request.URL.Query().Get("name")
 
 	for i := range data {
-		ownerMatch := owner == "" || owner == data[i].Owner
+		regionMatch := region == "" || region == data[i].Region
+		locomotionMatch := locomotion == "" || locomotion == data[i].Locomotion
+		foodMatch := food == "" || food == data[i].Food
 		nameMatch := name == "" || name == data[i].Name
 
-		if ownerMatch && nameMatch {
+		if regionMatch && locomotionMatch && foodMatch && nameMatch {
 			result = append(result, data[i])
 		}
 	}
--- static/main.js
+++ static/main.js
@@ -1,20 +1,11 @@
 const itemsDiv = document.getElementById('itemsDiv');
-const ownerField = document.getElementById('ownerField');
-const nameField = document.getElementById('nameField');
-const ownerButton = document.getElementById('ownerButton');
+const regionField = document.getElementById('regionField');
+const locomotionField = document.getElementById('locomotionField');
+const foodField = document.getElementById('foodField');
+const filterButton = document.getElementById('filterButton');
 
-async function fetchDinos(owner, name){
-    const response = await fetch('/dino?owner=' + owner + '&name=' + name);
-
-    if(!response.ok){
-        throw await response.text();
-    }
-
-    return await response.json()
-}
-
-async function fetchDinoNames(){
-    const response = await fetch('/dino/name');
+async function fetchDinos(region = '', locomotion = '', food = '', name = ''){
+    const response = await fetch('/api/dino?region=' + region + '&locomotion=' + locomotion + '&food=' + food + '&name=' + name);
 
     if(!response.ok){
         throw await response.text();
@@ -23,20 +14,20 @@ async function fetchDinoNames(){
     return await response.json()
 }
 
-async function fetchOwners(){
-    const response = await fetch('/owner');
+async function fetchCategories(){
+    const response = await fetch('/api/dino/category');
 
     if(!response.ok){
         throw await response.text();
     }
 
-    return await response.json()
+    return await response.json();
 }
 
 async function loadData(){
     let data;
     try{
-        data = await fetchDinos(ownerField.value, nameField.value);
+        data = await fetchDinos(regionField.value, locomotionField.value, foodField.value);
     }catch(e){
         alert(e);
         return;
@@ -46,14 +37,15 @@ async function loadData(){
 
     const table = document.createElement('table');
 
-    const fields = [
-      { name: "Name", label: "Nome" },
-      { name: "Action", label: "Ação" },
-      { name: "Owner", label: "Dono" },
+    const categoryNames = [
+      { name: "name", label: "Nome" },
+      { name: "food", label: "Alimentação" },
+      { name: "locomotion", label: "Locomoção" },
+      { name: "region", label: "Região" }
     ];
 
     const trTitle = document.createElement("tr");
-    for(const f of fields){
+    for(const f of categoryNames){
         const th = document.createElement('th');
         th.innerText = f.label;
         trTitle.appendChild(th);
@@ -62,7 +54,7 @@ async function loadData(){
     
     for(const obj of data){
         const tr = document.createElement('tr');
-        for(const f of fields){
+        for(const f of categoryNames){
             const td = document.createElement('td');
             td.innerText = obj[f.name];
             tr.appendChild(td);
@@ -73,18 +65,10 @@ async function loadData(){
     itemsDiv.appendChild(table);
 }
 
-async function createOwnersDropdown(){
-    let owners;
-    try{
-        owners = await fetchOwners();
-    }
-    catch(e){
-        alert(e);
-        return;
-    }
-
-    const optionsKeys =  ['Todos', ...owners];
-    const optionsValues = ['', ...owners];
+function createFoodDropdown(categories){
+    const {foods} = categories;
+    const optionsKeys =  ['Todos', ...foods];
+    const optionsValues = ['', ...foods];
 
     for(let i = 0; i < optionsKeys.length; i++){
         const key = optionsKeys[i];
@@ -92,22 +76,14 @@ async function createOwnersDropdown(){
         const opt = document.createElement('option');
         opt.innerText = key;
         opt.value = value;
-        ownerField.appendChild(opt);
+        foodField.appendChild(opt);
     }
 }
 
-async function createDinoNamesDropdown(){
-    let names;
-    try{
-        names = await fetchDinoNames();
-    }
-    catch(e){
-        alert(e);
-        return;
-    }
-
-    const optionsKeys =  ['Todos', ...names];
-    const optionsValues = ['', ...names];
+function createLocomotionDropdown(categories){
+    const {locomotions} = categories;
+    const optionsKeys =  ['Todos', ...locomotions];
+    const optionsValues = ['', ...locomotions];
 
     for(let i = 0; i < optionsKeys.length; i++){
         const key = optionsKeys[i];
@@ -115,15 +91,42 @@ async function createDinoNamesDropdown(){
         const opt = document.createElement('option');
         opt.innerText = key;
         opt.value = value;
-        nameField.appendChild(opt);
+        locomotionField.appendChild(opt);
+    }
+}
+
+function createRegionDropdown(categories){
+    const {regions} = categories;
+    const optionsKeys =  ['Todos', ...regions];
+    const optionsValues = ['', ...regions];
+
+    for(let i = 0; i < optionsKeys.length; i++){
+        const key = optionsKeys[i];
+        const value = optionsValues[i];
+        const opt = document.createElement('option');
+        opt.innerText = key;
+        opt.value = value;
+        regionField.appendChild(opt);
     }
 }
 
 async function main(){
-    await createOwnersDropdown();
-    await createDinoNamesDropdown();
+    
+    let categories;
+
+    try{
+        categories = await fetchCategories();
+    }
+    catch(e){
+        alert(e);
+        return;
+    }
+    
+    createRegionDropdown(categories);
+    createLocomotionDropdown(categories);
+    createFoodDropdown(categories);
 }
 
 main();
 
-ownerButton.onclick = loadData;
+filterButton.onclick = loadData;
