e0c5f73003442ec005a451922a09813434dc6eb5
João Hudson <joao.oliveira@projecti.com.br>
2022-07-01 15:56:13 -0300

    atualizando tabelas

--- sql/v1_create_tables.sql
+++ sql/v1_create_tables.sql
@@ -1,9 +1,25 @@
+-- Forma como o dino se locomove
+CREATE TABLE locomotion(
+    id_locomotion BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name_locomotion VARCHAR(80) NOT NULL
+);
+-- Região onde se pode encontrar o dino
+CREATE TABLE region(
+    id_region BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name_region VARCHAR(80) NOT NULL
+);
+-- Tipo de Alimentação
+CREATE TABLE food(
+    id_food BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name_food VARCHAR(80) NOT NULL
+);
+-- Dados gerais do dinossauro
 CREATE TABLE dino(
-    id_dino BIGINT GENERATED ALWAYS AS IDENTITY,
+    id_dino BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name_dino VARCHAR(80) NOT NULL,
-    food_dino VARCHAR(80) NOT NULL,
-    locomotion_dino VARCHAR(30) NOT NULL,
-    region_dino VARCHAR(80) NOT NULL,
+    id_region BIGINT REFERENCES region(id_region),
+    id_food BIGINT REFERENCES food(id_food),
+    id_locomotion BIGINT REFERENCES locomotion(id_locomotion),
     utility_dino VARCHAR(300) NOT NULL,
     training_dino TEXT NOT NULL
 );
