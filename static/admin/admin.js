fa1776cc6c438cd8af1780bc9c6496d4ce14b7f9
João Hudson <joao.oliveira@projecti.com.br>
2022-07-05 11:21:42 -0300

    adicionando tela de criacao de dino

---
+++ static/admin/admin.js
@@ -0,0 +1,83 @@
+const regionField = document.getElementById('regionField');
+const locomotionField = document.getElementById('locomotionField');
+const foodField = document.getElementById('foodField');
+const utitlityField = document.getElementById('utilityField');
+const trainingField = document.getElementById('trainingField');
+const nameField = document.getElementById('nameField');
+const createDinoButton = document.getElementById('createDinoButton');
+
+async function fetchCategories(){
+    const response = await fetch('/api/dino/categories');
+
+    if(!response.ok){
+        throw await response.text();
+    }
+
+    return await response.json();
+}
+
+async function postDino(dino){
+    const response = await fetch('/api/dino', {method: 'POST', body: JSON.stringify(dino)});
+
+    if(!response.ok){
+        throw await response.text();
+    }
+}
+
+async function createDino(){
+    const dino = {
+        name: nameField.value,
+        utility: utitlityField.value,
+        training: trainingField.value,
+        regionId: new Number(regionField.value),
+        locomotionId: new Number(locomotionField.value),
+        foodId: new Number(foodField.value)
+    };
+
+    try{
+        await postDino(dino);
+    }
+    catch(e){
+        alert(e);
+        return;
+    }
+
+    alert('Dino criado com sucesso!');
+}
+
+function pupulateDropdown(data, dropdown){
+    const keys = data.map(k => k.name);
+    const values = data.map(v => v.id);
+    const optionsKeys =  keys;
+    const optionsValues = values;
+
+    for(let i = 0; i < optionsKeys.length; i++){
+        const key = optionsKeys[i];
+        const value = optionsValues[i];
+        const opt = document.createElement('option');
+        opt.innerText = key;
+        opt.value = value;
+        dropdown.appendChild(opt);
+    }
+}
+
+async function main(){
+    let categories;
+
+    try{
+        categories = await fetchCategories();
+    }
+    catch(e){
+        alert(e);
+        return;
+    }
+
+    pupulateDropdown(categories.regions, regionField);
+    pupulateDropdown(categories.locomotions, locomotionField);
+    pupulateDropdown(categories.foods, foodField);
+}
+
+main();
+
+
+createDinoButton.onclick = createDino;
