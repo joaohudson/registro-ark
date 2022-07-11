8cc749523f890398b2745ac16586737c07409051
João Hudson <joao.oliveira@projecti.com.br>
2022-07-11 16:34:32 -0300

    concluindo gerenciamento de categorias no frontend

--- static/admin/dino/category/category.js
+++ static/admin/dino/category/category.js
@@ -19,6 +19,14 @@ async function postLocomotion(name){
     }
 }
 
+async function deleteLocomotion(id){
+    const response = await fetch('/api/dino/category/locomotion?id=' + id, {method: 'DELETE'});
+
+    if(!response.ok){
+        throw await response.text();
+    }
+}
+
 async function postRegion(name){
     const body = JSON.stringify({name});
     const response = await fetch('/api/dino/category/region', {method: 'POST', body: body});
@@ -28,9 +36,25 @@ async function postRegion(name){
     }
 }
 
+async function deleteRegion(id){
+    const response = await fetch('/api/dino/category/region?id=' + id, {method: 'DELETE'});
+
+    if(!response.ok){
+        throw await response.text();
+    }
+}
+
 async function postFood(name){
     const body = JSON.stringify({name});
     const response = await fetch('/api/dino/category/food', {method: 'POST', body: body});
+
+    if(!response.ok){
+        throw await response.text();
+    }
+}
+
+async function deleteFood(id){
+    const response = await fetch('/api/dino/category/food?id=' + id, {method: 'DELETE'});
 
     if(!response.ok){
         throw await response.text();
@@ -47,6 +71,32 @@ async function fetchCategories(){
     return await response.json();
 }
 
+function populateListCategories(data, list, deleteFunc){
+    list.innerHTML = '';
+    for(const category of data){
+        const li = document.createElement('li');
+        const span = document.createElement('span');
+        span.innerText = category.name;
+        li.appendChild(span);
+        const button = document.createElement('button');
+        button.innerText = '❌';
+        button.onclick = async () => {
+            try{
+                const ok = await dialog.showConfirm('Tem certeza que deseja deletar ' + category.name + ' ?');
+                if(ok)
+                    await deleteFunc(category.id);
+                await loadData();
+            }
+            catch(e){
+                dialog.showMessage(e);
+                return;
+            }
+        };
+        li.appendChild(button);
+        list.appendChild(li);
+    }
+}
+
 async function loadData(){
     let categories;
 
@@ -58,26 +108,9 @@ async function loadData(){
         return;
     }
 
-    foodList.innerHTML = '';
-    for(const food of categories.foods){
-        const li = document.createElement('li');
-        li.innerText = food.name;
-        foodList.appendChild(li);
-    }
-
-    regionList.innerHTML = '';
-    for(const region of categories.regions){
-        const li = document.createElement('li');
-        li.innerText = region.name;
-        regionList.appendChild(li);
-    }
-
-    locomotionList.innerHTML = '';
-    for(const locomotion of categories.locomotions){
-        const li = document.createElement('li');
-        li.innerText = locomotion.name;
-        locomotionList.appendChild(li);
-    }
+    populateListCategories(categories.foods, foodList, deleteFood);
+    populateListCategories(categories.regions, regionList, deleteRegion);
+    populateListCategories(categories.locomotions, locomotionList, deleteLocomotion);
 }
 
 async function main(){
@@ -88,6 +121,7 @@ locomotionButton.onclick = async () => {
     try{
         await postLocomotion(locomotionField.value);
         await loadData();
+        locomotionField.value = '';
     }
     catch(e){
         dialog.showMessage(e);
@@ -99,6 +133,7 @@ regionButton.onclick = async () => {
     try{
         await postRegion(regionField.value);
         await loadData();
+        regionField.value = '';
     }
     catch(e){
         dialog.showMessage(e);
@@ -110,6 +145,7 @@ foodButton.onclick = async () => {
     try{
         await postFood(foodField.value);
         await loadData();
+        foodField.value = '';
     }
     catch(e){
         dialog.showMessage(e);
