e2aba8023b4872d7ef0f541d5e252f439f4fa00e
João Hudson <joao.oliveira@projecti.com.br>
2022-07-15 13:07:54 -0300

    melhorando tela inicial

--- static/admin/admin.js
+++ static/admin/admin.js
@@ -1,8 +1,41 @@
 (function (){
 
-const token = localStorage.getItem('token');
-if(!token){
-    location.href = '/admin/login';
+const nameLabel = document.getElementById('nameLabel');
+
+const addDinoRoute = document.getElementById('addDinoRoute');
+const listDinoRoute = document.getElementById('listDinoRoute');
+const managerCategoryRoute = document.getElementById('managerCategoryRoute');
+const managerAdmRoute = document.getElementById('managerAdmRoute');
+const logoutButton = document.getElementById('logoutButton');
+
+async function fetchAdm(){
+    return await adminFetch('/api/adm', {}, true);
 }
+
+logoutButton.onclick = () => {
+    delete localStorage.token;
+    location.href = '/admin/login';
+};
+
+async function main(){
+    try{
+        const adm = await fetchAdm();
+
+        nameLabel.innerText = 'Adm: ' + adm.name;
+
+        addDinoRoute.hidden = !adm.permissionManagerDino;
+        listDinoRoute.hidden = !adm.permissionManagerDino;
+
+        managerCategoryRoute.hidden = !adm.permissionManagerCategory;
+
+        managerAdmRoute.hidden = !adm.permissionManagerAdm;
+    }
+    catch(e){
+        dialog.showMessage(e);
+        return;
+    }
+}
+
+main();
 
 })();
